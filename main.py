1965a16286cb07dd74d1d5e5967ccb1cadc5f61e
BirWri <55280458+BirWri@users.noreply.github.com>
2022-07-04 11:54:11 +0200

    turtles, movement, winner check

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+from turtle import Turtle, Screen
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+screen = Screen()
+
+is_race_on = True
+screen.setup(width=500, height=400)
+user_choice = screen.textinput(title="Make your bet!", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [80, 50, 20, -10, -40, -70]
+all_turtles = []
+
+for turtle_index in range(0,6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_choice:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_choice:
+                print("You won!")
+            else:
+                print(f"You lose. The winning color was {winning_color}")
+
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+screen.exitonclick()
